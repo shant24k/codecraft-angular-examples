4e80b8365697808d856a6b61bfb0962c6e21ff34
Shantanu Shashikant Kulkarni <Shantanu.Kulkarni@wu.com>
2018-08-26 01:18:22 +0530

    Model Driven Forms

---
+++ FORMS/Model-Driven-Forms/src/app/model-form/model-form.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+
+@Component({
+  selector: 'app-model-form',
+  templateUrl: './model-form.component.html',
+  styleUrls: ['./model-form.component.css']
+})
+export class ModelFormComponent implements OnInit {
+
+  langs: string[] = ["English", "French", "German"];
+  myform: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
+
+  createFormControls() {
+    this.firstName = new FormControl("", Validators.required);
+    this.lastName = new FormControl("", Validators.required);
+    this.email = new FormControl("", [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ]);
+    this.password = new FormControl("", [
+      Validators.required,
+      Validators.minLength(8)
+    ]);
+    this.language = new FormControl("", Validators.required);
+  }
+
+  createForm() {
+    this.myform = new FormGroup({
+      name: new FormGroup({
+        firstName: this.firstName,
+        lastName: this.lastName
+      }),
+      email: this.email,
+      password: this.password,
+      language: this.language
+    });
+  }
+
+  ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  onSubmit() {
+    if (this.myform.valid) {
+      console.log("Form Submitted!");
+      console.log(this.myform.value);
+      this.myform.reset();
+    }
+  }
+
+}
