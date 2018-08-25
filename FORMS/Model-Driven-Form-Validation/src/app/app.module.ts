f60445660718974c68d7430b4b4119e15b9e6d5f
Shantanu Shashikant Kulkarni <Shantanu.Kulkarni@wu.com>
2018-08-26 01:49:48 +0530

    Model Driven Form Validation

---
+++ FORMS/Model-Driven-Form-Validation/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormsModule
+} from "@angular/forms";
+
+import { AppComponent } from './app.component';
+import { ModelFormComponent } from './model-form/model-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ModelFormComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule, ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
