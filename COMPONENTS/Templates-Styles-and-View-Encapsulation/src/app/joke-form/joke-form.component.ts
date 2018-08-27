5da91f449faea72c5cc035497b034df3223a1d79
Shantanu Shashikant Kulkarni <Shantanu.Kulkarni@wu.com>
2018-08-28 01:01:44 +0530

    Templates Styles and View Encapsulation

---
+++ COMPONENTS/Templates-Styles-and-View-Encapsulation/src/app/joke-form/joke-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Joke } from '../joke';
+
+@Component({
+  selector: 'app-joke-form',
+  templateUrl: './joke-form.component.html',
+  styleUrls: ['./joke-form.component.css']
+  // encapsulation: ViewEncapsulation.Emulated
+  // encapsulation: ViewEncapsulation.Native
+  // encapsulation: ViewEncapsulation.None
+})
+export class JokeFormComponent implements OnInit {
+
+  @Output() jokeCreated = new EventEmitter<Joke>();
+
+  createJoke(setup: string, punchline: string) {
+    this.jokeCreated.emit(new Joke(setup, punchline));
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
