515246e36498bc072ca9fad88abb7b4235249bde
Shantanu Shashikant Kulkarni <Shantanu.Kulkarni@wu.com>
2018-08-27 14:25:49 +0530

    NgIf and NgSwitch Example

---
+++ BUILT-IN-DIRECTIVES/NgIf-and-NgSwitch/src/app/ng-if-example/ng-if-example.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-if-example',
+  templateUrl: './ng-if-example.component.html',
+  styleUrls: ['./ng-if-example.component.css']
+})
+export class NgIfExampleComponent implements OnInit {
+
+  constructor() { }
+
+  people: any[] = [
+    {
+      "name": "Douglas  Pace",
+      "age": 35
+    },
+    {
+      "name": "Mcleod  Mueller",
+      "age": 32
+    },
+    {
+      "name": "Day  Meyers",
+      "age": 21
+    },
+    {
+      "name": "Aguirre  Ellis",
+      "age": 34
+    },
+    {
+      "name": "Cook  Tyson",
+      "age": 32
+    }
+  ];
+
+  ngOnInit() {
+  }
+
+}
