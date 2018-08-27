dbc8ae0ee29c13738599021c679e4f7c048755ed
Shantanu Shashikant Kulkarni <Shantanu.Kulkarni@wu.com>
2018-08-27 22:59:56 +0530

    Inputs and Configuration

---
+++ CUSTOM-DIRECTIVES/Inputs-and-Configuration/src/app/card-hover.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, Input, HostBinding, ElementRef, Renderer, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appCardHover]'
+})
+export class CardHoverDirective {
+
+  @Input("appCardHover")
+  config: any = {
+    querySelector: ".card-text"
+  };
+
+  @HostBinding("class.border-success") private ishovering: boolean;
+
+  constructor(private el: ElementRef, private renderer: Renderer) {}
+
+  @HostListener("mouseover")
+  onMouseOver() {
+    let part = this.el.nativeElement.querySelector(this.config.querySelector);
+    this.renderer.setElementStyle(part, "display", "block");
+    this.ishovering = true;
+  }
+
+  @HostListener("mouseout")
+  onMouseOut() {
+    let part = this.el.nativeElement.querySelector(this.config.querySelector);
+    this.renderer.setElementStyle(part, "display", "none");
+    this.ishovering = false;
+  }
+
+}
