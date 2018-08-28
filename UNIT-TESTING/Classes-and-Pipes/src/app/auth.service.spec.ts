3e22fc2d66bbf2028128c48f1d1e183d573d877e
Shantanu Shashikant Kulkarni <Shantanu.Kulkarni@wu.com>
2018-08-28 13:05:07 +0530

    Classes and Pipes Unit Testing Example

---
+++ UNIT-TESTING/Classes-and-Pipes/src/app/auth.service.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AuthService]
+    });
+  });
+
+  it('should be created', inject([AuthService], (service: AuthService) => {
+    expect(service).toBeTruthy();
+  }));
+});
+
+describe('Service: Auth', () => {
+
+  let service: AuthService;
+
+  beforeEach(() => {
+    service = new AuthService();
+  });
+
+  afterEach(() => {
+    service = null;
+    localStorage.removeItem('token');
+  });
+
+  it('should return true from isAuthenticated when there is a token', () => {
+    localStorage.setItem('token', '1234');
+    expect(service.isAuthenticated()).toBeTruthy();
+  });
+
+  it('should return false from isAuthenticated when there is no token', () => {
+    expect(service.isAuthenticated()).toBeFalsy();
+  });
+
+});
